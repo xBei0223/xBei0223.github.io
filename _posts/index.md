de012d0b175866ef8788d85b777bb0f7caebf627
xbei <jiyaa0223@outlook.com>
2024-07-04 07:12:03 +0800

    Just test

---
+++ _posts/index.md
@@ -0,0 +1,9 @@
+---
+layout: default
+title: Home
+---
+
+<div id="app">
+  <p>{{ message }}</p>
+</div>
+ 
---
+++ assets/main.js
@@ -0,0 +1,8 @@
+// assets/main.js
+new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue.js with Jekyll!'
+    }
+  });
+  
